59ff733540d1c1f8bdffc8b696db445e5924c665
IbtidaB01 <2105061@cse.buet.ac.bd>
2023-07-14 09:11:38 +0600

    change

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+function generateVaccinationDate() {
+  const startDate = new Date(); // Set the start date as today
+  const endDate = new Date();
+  endDate.setDate(startDate.getDate() + 30); // Set the end date as 30 days from today
+
+  const vaccinationDate = new Date(
+    startDate.getTime() + Math.random() * (endDate.getTime() - startDate.getTime())
+  );
+
+  return vaccinationDate.toISOString(); // Convert the date to ISO string format
+}
+
 // GET route to display the registration form
 app.get('/register', (req, res) => {
   res.sendFile(__dirname + '/register.html');
@@ -15,9 +27,31 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
   const { name, address, nationID } = req.body;
 
-  // Perform necessary actions (e.g., store user data, validation, etc.)
+  // Validate required fields
+  if (!name || !address || !nationID) {
+    res.status(400).send('Required fields are missing');
+    return;
+  }
 
-  res.send('User registered successfully!');
+  // Validate input formats
+  if (nationID.length !== 6 || !/^[A-Za-z0-9]+$/.test(nationID)) {
+    res.status(400).send('Invalid input formats');
+    return;
+  }
+
+  // Perform necessary actions for registration (e.g., store user data, validation, etc.)
+
+  // Generate vaccination date
+  const vaccinationDate = generateVaccinationDate();
+
+  // Return response with vaccination date
+  res.json({ message: 'User registered successfully!', vaccinationDate });
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 module.exports = app;
--- test/registration.test.js
+++ test/registration.test.js
@@ -1,11 +1,73 @@
 const request = require('supertest');
-const server = require('../server');
+const app = require('../index');
 
+// Close the server function
+const closeServer = (server) => {
+  server.close();
+};
+
+// Legacy tests
 test('registers a user', async () => {
-  const response = await request(server)
+  const response = await request(app)
     .post('/register')
     .send({ name: 'John Doe', address: '123 Main St', nationID: 'ABC123' });
 
   expect(response.statusCode).toBe(200);
-  expect(response.text).toBe('User registered successfully!');
+  expect(response.body.message).toBe('User registered successfully!');
+});
+
+test('displays the registration form', async () => {
+  const response = await request(app).get('/register');
+  expect(response.statusCode).toBe(200);
+  // Add more assertions to verify the form elements
+});
+
+test('validates required fields', async () => {
+  const response = await request(app)
+    .post('/register')
+    .send({ name: '', address: '', nationID: '' });
+  expect(response.statusCode).toBe(400);
+  // Add more assertions to verify the error response
+});
+
+test('validates input formats', async () => {
+  const response = await request(app)
+    .post('/register')
+    .send({ name: 'John Doe', address: '123 Main St', nationID: 'invalid' });
+  expect(response.statusCode).toBe(400);
+  // Add more assertions to verify the error response
+});
+
+test('prevents duplicate registration', async () => {
+  // Perform a registration request with a specific nationID
+  // Perform another registration request with the same nationID
+  // Verify the second request returns an appropriate error response
+});
+
+// New tests
+
+// Test case for generating a vaccination date
+test('generates a vaccination date', async () => {
+  const response = await request(app)
+    .post('/register')
+    .send({ name: 'John Doe', address: '123 Main St', nationID: 'ABC123' });
+
+  expect(response.statusCode).toBe(200);
+  expect(response.body.message).toBe('User registered successfully!');
+  expect(response.body).toHaveProperty('vaccinationDate');
+  // Add more assertions to verify the format and validity of the vaccination date
+});
+
+// Add more new tests as needed
+
+// Close the server after the tests are completed
+let server;
+
+beforeAll((done) => {
+  server = app.listen(done);
+});
+
+afterAll((done) => {
+  closeServer(server);
+  done();
 });
